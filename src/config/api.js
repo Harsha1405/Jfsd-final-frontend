bfa566fce7c2d7c28645bff49d8747e4bd3b2d96
Harsha1405 <137099244+Harsha1405@users.noreply.github.com>
2024-12-08 00:23:11 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
 const LOCALHOST='http://localhost:2024'
 
-export const API_BASE_URL =LOCALHOST
+export const API_BASE_URL ="https://jfsd-final-backend-production.up.railway.app"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
