0ec5f5a173372bf5e9f8ab76a089474cd6e7bd76
Harsha Vardhan Pavan Sai <2200030060@kluniversity.in>
2024-12-07 23:27:05 +0530

    first commit

---
+++ src/customer/Components/Auth/Register.jsx
@@ -0,0 +1,181 @@
+import {
+  Grid,
+  TextField,
+  Button,
+  Box,
+  Snackbar,
+  Alert,
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, register } from "../../../Redux/Auth/Action";
+import { Fragment, useEffect, useState } from "react";
+
+export default function RegisterUserForm({ handleNext }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+  const [errors, setErrors] = useState({});
+  const { auth } = useSelector((store) => store);
+
+  const handleClose = () => setOpenSnackBar(false);
+  const jwt = localStorage.getItem("jwt");
+
+  useEffect(() => {
+    if (jwt) {
+      dispatch(getUser(jwt));
+    }
+  }, [jwt]);
+
+  useEffect(() => {
+    if (auth.user || auth.error) setOpenSnackBar(true);
+  }, [auth.user, auth.error]);
+
+  const validate = (data) => {
+    const newErrors = {};
+
+    if (!data.firstName) newErrors.firstName = "First name is required";
+    else if (!/^[a-zA-Z]+$/.test(data.firstName)) newErrors.firstName = "First name can only contain letters";
+
+    if (!data.lastName) newErrors.lastName = "Last name is required";
+    else if (!/^[a-zA-Z]+$/.test(data.lastName)) newErrors.lastName = "Last name can only contain letters";
+
+    if (!data.email) newErrors.email = "Email is required";
+    else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) newErrors.email = "Invalid email format";
+
+    if (!data.password) newErrors.password = "Password is required";
+    else if (data.password.length < 6) newErrors.password = "Password must be at least 6 characters";
+
+    if (!data.role) newErrors.role = "Role is required";
+
+    return newErrors;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const userData = {
+      firstName: data.get("firstName"),
+      lastName: data.get("lastName"),
+      email: data.get("email"),
+      password: data.get("password"),
+      role: data.get("role"),
+    };
+
+    const validationErrors = validate(userData);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    setErrors({});
+    console.log("user data", userData);
+    dispatch(register(userData));
+  };
+
+  return (
+    <div className="">
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              fullWidth
+              autoComplete="given-name"
+              error={!!errors.firstName}
+              helperText={errors.firstName}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              fullWidth
+              autoComplete="family-name"
+              error={!!errors.lastName}
+              helperText={errors.lastName}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Email"
+              fullWidth
+              autoComplete="email"
+              error={!!errors.email}
+              helperText={errors.email}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <FormControl fullWidth error={!!errors.role}>
+              <InputLabel id="demo-simple-select-label">Role</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Role"
+                name="role"
+              >
+                <MenuItem value={"ROLE_ADMIN"}>Admin</MenuItem>
+                <MenuItem value={"ROLE_CUSTOMER"}>Customer</MenuItem>
+              </Select>
+              {errors.role && <p style={{ color: 'red', margin: 0, fontSize: '0.75rem' }}>{errors.role}</p>}
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="password"
+              name="password"
+              label="Password"
+              fullWidth
+              autoComplete="new-password"
+              type="password"
+              error={!!errors.password}
+              helperText={errors.password}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              className="bg-[#9155FD] w-full"
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{ padding: ".8rem 0" }}
+            >
+              Register
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+
+      <div className="flex justify-center flex-col items-center">
+        <div className="py-3 flex items-center ">
+          <p className="m-0 p-0">If you already have an account?</p>
+          <Button onClick={() => navigate("/login")} className="ml-5" size="small">
+            Login
+          </Button>
+        </div>
+      </div>
+
+      <Snackbar open={openSnackBar} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={auth.error ? "error" : "success"} sx={{ width: '100%' }}>
+          {auth.error ? auth.error : auth.user ? "Register Success" : ""}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
